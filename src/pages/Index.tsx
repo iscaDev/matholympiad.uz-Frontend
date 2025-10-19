21f5eba32766eb9063f958785d277d02f31d7591
Sardorbek Norqulov <152975289+Norqulov-Sardorbek@users.noreply.github.com>
2025-10-19 20:07:21 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,14 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
+import Article from '@/components/ArticlesSection';
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background font-sans">
       <Navbar />
       <Hero />
+      <Article/>
       <Footer />
     </div>
   );
