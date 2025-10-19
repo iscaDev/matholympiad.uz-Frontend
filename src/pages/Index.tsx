c1a89b00a85582d1fa7f076102f19407eca7e811
Isfandiyor Mullajonov <114288417+iscaDev@users.noreply.github.com>
2025-10-19 20:48:44 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,14 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
-import Article from '@/components/RecentArticles';
+import articles from '@/components/RecentArticles';
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background font-sans">
       <Navbar />
       <Hero />
+      <articles />
       <Footer />
     </div>
   );
